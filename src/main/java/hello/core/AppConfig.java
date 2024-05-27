acb9a24c4c3f7dc289a4c4b180b5af5e8fd5ad74
kimeomji333 <148833676+kimeomji333@users.noreply.github.com>
2024-05-28 00:57:21 +0900

    안쓰는 코드 정리

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,17 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
-import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// 기존에는 AppConfig 에서 직접 객체를 생성하고 DI를 했지만, 지금부터는 스프링 컨테이너를 사용
+// 스프링 컨테이너를 사용
 // Bean 메서드를 호출해서 반환된 객체를 스프링 컨테이너에 등록한다.
 @Configuration
 public class AppConfig {
