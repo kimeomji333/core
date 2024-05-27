608e2814dfce16aad59d1f8d04b2707957e839ba
kimeomji333 <148833676+kimeomji333@users.noreply.github.com>
2024-05-27 12:42:21 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,10 +6,13 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
+// 새로운 할인 정책으로 변경하려면, OrderServiceImpl 클래스 코드를 고쳐야 한다.
+// 클래스의 의존 관계를 보면, 추상(인터페이스) DiscountPolicy, 구현 클래스 DiscountPolicy 에 의존하는 상태
+// private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); -> 수정
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private DiscountPolicy discountPolicy; // 인터페이스만 의존하도록 코드 변경
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
